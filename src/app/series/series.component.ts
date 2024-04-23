92d36dbd80a9b26cf45a56ff43fadfe065103942
Julian Mondragon <j.mondragon1@uniandes.edu.co>
2024-04-22 22:58:53 -0500

    Seasons

--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -12,14 +12,26 @@ export class SeriesComponent implements OnInit {
 
   constructor(private serieService: SerieService) { }
   series: Array<Serie> = [];
+  average_seasons = 0;
   getSeriesList() {
     this.serieService.getSeries().subscribe(series => {
       this.series = series;
     });
   }
 
+  average(){
+    let total: number=0;
+    console.log(this.series.length);
+    for(let index = 0; index< this.series.length; index++){
+        let serie: Serie = this.series[index]
+        total += serie.seasons;
+    }
+    this.average_seasons = total / this.series.length
+  }
+
   ngOnInit() {
     this.getSeriesList();
+    this.average();
   }
 
 }
